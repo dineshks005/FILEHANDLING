dd01fabe07068c1d431f90281de8ed95ed3da969
dineshks005 <dineshsubramani005@gmail.com>
2024-10-23 18:48:44 +0530

    files added

---
+++ src/main/java/com/tripillar/filehandling/text/ReadTextFile.java
@@ -0,0 +1,24 @@
+package com.tripillar.filehandling.text;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+
+public class ReadTextFile {
+    public static void main(String[] args){
+        File myfile = new File("thistext.txt");
+
+        try {
+            Scanner sc = new Scanner(myfile);
+            while(sc.hasNextLine())
+            {
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
